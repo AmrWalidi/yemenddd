6fc944f8096aec3253a8a490ce77c8a9dec4bdea
Amr Walidi <amr.nawaf@gmail.com>
2022-09-03 13:17:19 +0300

    submenulist change

--- js/header.js
+++ js/header.js
@@ -1,10 +1,17 @@
 $(document).ready(function() {
-            $("#dropdown1").hover(function(){
-                $("#menu1").fadeToggle();
-               });
-            $("#dropdown2").hover(function(){
-                $("#menu2").fadeToggle();
-               });
+    window.resize();
 });
+
+
+$(window).resize(function(){
+if(window.innerWidth > 700){
+$("#dropdown1").hover(function(){
+    $("#menu1").fadeToggle();
+   });
+$("#dropdown2").hover(function(){
+    $("#menu2").fadeToggle();
+   });
+}
+})
 
      
